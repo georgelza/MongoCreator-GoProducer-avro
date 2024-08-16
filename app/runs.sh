8ebf0d9e67708ea7d032f975d89b2b5722a7bfb0
George Leonard <georgelza@gmail.com>
2024-08-16 21:28:25 +0200

    Cleaning .pwd and .pwdloc, they are not included in the git repo

--- app/runs.sh
+++ app/runs.sh
@@ -1,31 +1,5 @@
-# the Below file contains the following variables.
-# This is done as such to not include these values into the git repo sync.
-# The files themself are added/listed in the .gitignore file.
-# 
-# Mongo and Kafka credentials as local env variables
-# Kafka Creds
-#
-#export Sasl_username=               
-#export Sasl_password=
 
-# MongoDB ATLAS Creds
-# https://medium.com/mongoaudit/how-to-enable-authentication-on-mongodb-b9e8a924efac
-#
-#export mongo_username=    # Same as Atlas / Google auth georgelza@gmail.com
-#export mongo_password=
-#
 #. ./.pwd
-
-
-########################################################################
-# Golang  Examples : https://developer.confluent.io/get-started/go/
-
-########################################################################
-# MongoDB Params
-# Mongo -> Kafka -> MongoDB 
-# https://blog.ldtalentwork.com/2020/05/26/how-to-sync-your-mongodb-databases-using-kafka-and-mongodb-kafka-connector/
-#
-# *_mongo.json & -> See .pws
 
 #go run -v cmd/main.go avro
 /app/cmd/main avro
