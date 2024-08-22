babeee7931e0621f9f7480c43cb3cfcfafc13a75
George Leonard <georgelza@gmail.com>
2024-08-22 14:29:15 +0200

    updated the create foreign table statement to be iceberg relevant

--- paradedb/sql/t_salesbaskets.sql
+++ paradedb/sql/t_salesbaskets.sql
@@ -1,13 +1,25 @@
 
-
+-- Parquet files directly
 CREATE FOREIGN TABLE t_salesbaskets ()
 SERVER parquet_server
 OPTIONS (files 's3://iceberg/dev.db/t_salesbaskets/data/*.parquet');
 
+--
+-- or
+-- Iceberg tables as it points to the metadata and data files.
+CREATE FOREIGN TABLE t_salesbaskets ()
+SERVER parquet_server
+OPTIONS (
+    files 's3://iceberg/dev.db/t_salesbaskets/*'
+);
+
 select count(*) from t_salesbaskets;
 
-
-select store->>'id' from t_salesbaskets limit 5;
+select 
+	store->>'id' as store_id, 
+	clerk->>'name' as clerk_name,
+	clerk->>'surname' as clerk_surnname
+from t_salesbaskets limit 5;
 
 select  
 	basketitems[1]->>'brand' as brand,
@@ -16,7 +28,7 @@ select
 	basketitems[1]->>'price' as price,
 	basketitems[1]->>'quantity' as quantity,
 	round(CAST(basketitems[1]->>'price' as numeric)*CAST (basketitems[1]->>'quantity' as integer),2) as subtotal
-	from t_salesbaskets limit 5;
+from t_salesbaskets limit 5;
 
 
 
